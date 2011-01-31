ff40ae7ac7790a0cd3259464b3a5df411e0897bd
Johannes Wagener <johannes@soundcloud.com>
2011-01-31 08:55:38 -0800

    fix typo from last commits

--- lib/soundcloud/hash_response_wrapper.rb
+++ lib/soundcloud/hash_response_wrapper.rb
@@ -1,6 +1,6 @@
 class Soundcloud::HashResponseWrapper < Hashie::Mash
   attr_reader :response
-  def initialize(response *args)
+  def initialize(response, *args)
     super(response, *args)
     @response = response
   end
