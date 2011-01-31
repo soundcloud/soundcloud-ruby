0341104714d53d416a421c5dd714f2d622886a6f
Johannes Wagener <johannes@soundcloud.com>
2011-01-31 08:49:30 -0800

    hash_response_wrapper#new accepting any number of args

--- lib/soundcloud/hash_response_wrapper.rb
+++ lib/soundcloud/hash_response_wrapper.rb
@@ -1,7 +1,7 @@
 class Soundcloud::HashResponseWrapper < Hashie::Mash
   attr_reader :response
-  def initialize(response)
-    super(response)
+  def initialize(response *args)
+    super(response, *args)
     @response = response
   end
 end
