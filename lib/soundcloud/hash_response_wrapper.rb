bb87c72b8763430b7dcad7d8555abeee322c1675
Johannes Wagener <johannes@soundcloud.com>
2011-04-13 15:02:54 +0200

    reenabling returning nil for non existend properties/attributes

--- lib/soundcloud/hash_response_wrapper.rb
+++ lib/soundcloud/hash_response_wrapper.rb
@@ -1,9 +1,7 @@
 class Soundcloud::HashResponseWrapper < Hashie::Mash
   attr_reader :response
   def initialize(response, *args)
-    super(response, *args) do |x| 
-      raise NoMethodError
-    end
+    super(response, *args)
     @response = response
   end
 end
--- spec/soundcloud/hash_response_wrapper_spec.rb
+++ spec/soundcloud/hash_response_wrapper_spec.rb
@@ -5,13 +5,8 @@ describe Soundcloud::HashResponseWrapper do
     it "should provide a Mash with accessors for the key/values of the passed response" do
       Soundcloud::HashResponseWrapper.new({:foo => 'bar'}).foo.should == 'bar'
     end
-    
-    it "should not have nil accessors for non present keys" do
-      Soundcloud::HashResponseWrapper.new({:foo => 'bar'}).should raise_error
-    end
-    
   end
-  
+
   describe '#response' do
     it "should return the original response object" do
       Soundcloud::HashResponseWrapper.new({:foo => 'bar'}).response.should == {:foo => 'bar'}
