28975b46fd13649281e6e6e0ec4913824884e118
Johannes Wagener <johannes@wagener.cc>
2011-10-27 11:54:25 -0700

    hash/array response wrapper accepts no args, fixes issue with hashie 1.2

--- lib/soundcloud/array_response_wrapper.rb
+++ lib/soundcloud/array_response_wrapper.rb
@@ -1,6 +1,6 @@
 class Soundcloud::ArrayResponseWrapper < Array
   attr_reader :response
-  def initialize(response)
+  def initialize(response=[])
     mashes = response.map { |o| Hashie::Mash.new(o) }
     self.replace(mashes)
     @response = response
