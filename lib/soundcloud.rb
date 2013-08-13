0f074a050170fa9dd54a55091888b3f191c14c50
Erik Michaels-Ober <sferik@gmail.com>
2013-08-13 23:57:58 +0200

    Add back support for get/post/put/delete/head module functions

--- lib/soundcloud.rb
+++ lib/soundcloud.rb
@@ -15,6 +15,17 @@ module SoundCloud
   end
   module_function :new
 
+  def method_missing(method_name, *args, &block)
+    return super unless respond_to_missing?(method_name)
+    Client.send(method_name, *args, &block)
+  end
+  module_function :method_missing
+
+  def respond_to_missing?(method_name, include_private=false)
+    Client.respond_to?(method_name, include_private)
+  end
+  module_function :respond_to_missing?
+
 end
 
 Soundcloud = SoundCloud
